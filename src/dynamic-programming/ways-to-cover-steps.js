2d0b9c759ea1aad75720238f7fbf0328ac0e1165
ccmoura <christophermoura48@gmail.com>
2023-07-29 19:52:59 -0300

    feat: add function to count ways to cover steps using dynamic programming

---
+++ src/dynamic-programming/ways-to-cover-steps.js
@@ -0,0 +1,13 @@
+const cache = {}
+
+const waysToCoverSteps = (step) => {
+    if (step < 0) return 0
+    if (step === 0) return 1
+
+    if (cache[step]) return cache[step]
+    
+    cache[step] = waysToCoverSteps(step - 1) + waysToCoverSteps(step - 2) + waysToCoverSteps(step - 3)
+
+    return cache[step]
+    
+}
